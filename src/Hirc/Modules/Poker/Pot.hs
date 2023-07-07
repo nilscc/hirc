8bdce0502988d66ee4b91d16518a94f485a54bd0
Nils <mail@nils.cc>
2023-07-07 16:04:57 +0200

    Add new pot struct

---
+++ src/Hirc/Modules/Poker/Pot.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module Hirc.Modules.Poker.Pot where
+
+import Data.Map (Map (..))
+import Data.Map qualified as M
+import Data.Maybe (fromMaybe)
+import Data.Set (Set (..))
+import Data.Set qualified as S
+import Hirc.Modules.Poker.Bank (Money)
+import Hirc.Types.Connection (UserName)
+
+data Pot = Pot
+  { potPlayers :: Map UserName Money,
+    potCommunity :: Money
+  }
+  deriving (Eq, Show)
+
+empty = Pot {potPlayers = M.empty, potCommunity = 0}
+
+height :: Pot -> Money
+height = maximum . potPlayers
+
+size :: Pot -> Money
+size p = sum (potPlayers p) + potCommunity p
+
+toCall :: UserName -> Pot -> Money
+toCall u p =
+  height p - fromMaybe 0 (M.lookup u (potPlayers p))
+
+put :: UserName -> Money -> Pot -> Pot
+put u m p =
+  p
+    { potPlayers = M.alter (Just . maybe m (m +)) u (potPlayers p)
+    }
+
+fold :: UserName -> Pot -> Pot
+fold u p =
+  p
+    { potPlayers = M.delete u (potPlayers p),
+      potCommunity = potCommunity p + fromMaybe 0 (M.lookup u (potPlayers p))
+    }
+
+-- | Split 
+split :: UserName -> Pot -> Maybe (Pot, Pot)
+split u p
+  | Just m <- M.lookup u (potPlayers p) =
+      Just (p1, p2 m)
+  | otherwise =
+      Nothing
+  where
+    p1 =
+      p
+        { potPlayers = M.delete u (potPlayers p),
+          potCommunity = 0
+        }
+    p2 m =
+      p
+        { potPlayers = M.singleton u m,
+          potCommunity = potCommunity p
+        }
---
+++ test/Hirc/Modules/Poker/PotTests.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module Hirc.Modules.Poker.PotTests where
+
+import Hirc.Modules.Poker.Bank (Money)
+import Hirc.Modules.Poker.Pot (Pot (..))
+import Hirc.Modules.Poker.Pot qualified as P
+import Hirc.Types.Connection (UserName)
+import Test.Hspec
+import Test.Hspec.Expectations
+
+buildPot :: [(UserName, Money)] -> Pot
+buildPot = foldr (uncurry P.put) P.empty
+
+-- Main spec
+
+pokerPotSpec :: Spec
+pokerPotSpec = do
+  potProperties
+  potSplit
+
+-- Sub specs
+
+potProperties :: Spec
+potProperties = describe "Pot properties" $ do
+  let pot =
+        buildPot
+          [ ("p1", 100),
+            ("p2", 200)
+          ]
+
+  it "should calculate the correct pot height and size" $ do
+    P.height pot `shouldBe` 200
+    P.size pot `shouldBe` 300
+
+  it "should calculate correct call amount" $ do
+    P.toCall "p1" pot `shouldBe` 100
+    P.toCall "p2" pot `shouldBe` 0
+    P.toCall "??" pot `shouldBe` 200
+
+  it "should move player pot to community pot on fold" $ do
+    P.fold "p1" pot `shouldSatisfy` (100 ==) . potCommunity
+    P.fold "p2" pot `shouldSatisfy` (200 ==) . potCommunity
+    P.fold "??" pot `shouldSatisfy` (0 ==) . potCommunity
+
+potSplit :: Spec
+potSplit = describe "Split pot" $ do
+  let p1 = ("p1", 100)
+      p2 = ("p2", 100)
+      p3 = ("p3", 200)
+      pot = buildPot [p1, p2, p3]
+
+  it "should move correct player" $ do
+    P.split "p3" pot
+      `shouldBe` Just
+        ( buildPot [p1, p2],
+          buildPot [p3]
+        )
+
+  it "should update community pot correctly" $ do
+    P.split "p3" pot {potCommunity = 50}
+      `shouldBe` Just
+        ( buildPot [p1, p3],
+          (buildPot [p2]) {potCommunity = 50}
+        )
