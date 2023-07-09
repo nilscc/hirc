5a09bc7bf653e3f301ebb5a33a67322dcf68c639
Nils <mail@nils.cc>
2023-07-09 16:06:54 +0200

    Fix admin module

--- app/Hirc/Modules/Admin.hs
+++ app/Hirc/Modules/Admin.hs
@@ -1,23 +1,22 @@
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
 
 {-# OPTIONS -fno-warn-incomplete-patterns #-}
 
 module Hirc.Modules.Admin
-  ( adminModule
-  , AdminSettings (..), emptyAdminSettings
-  ) where
-
-import qualified Data.Acid as AS
-import qualified Data.Map as M
-import qualified Data.ByteString.Char8 as B8
+  ( adminModule,
+    AdminSettings (..),
+    emptyAdminSettings,
+  )
+where
 
 import Control.Concurrent.STM (readTVarIO)
-import Control.Monad.IO.Class (MonadIO(..))
-
+import Control.Monad.IO.Class (MonadIO (..))
+import qualified Data.Acid as AS
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.Map as M
 import Hirc
 import Hirc.Modules.Admin.Acid
-
 
 --------------------------------------------------------------------------------
 -- Main module
@@ -26,12 +25,14 @@ runAdminModule :: AdminM ()
 runAdminModule = do
   currentChannel <- getCurrentChannel
   onValidPrefix $ do
-    userCommand $ \"admin" "auth" pw         -> auth pw
-    userCommand $ \"admin" "join" channel    -> requireAuth $ joinChannel channel
-    userCommand $ \"admin" "part" channel    -> doneAfter $ requireAuth $ partChannel channel
-    userCommand $ \"admin" "part"            -> doneAfter $ requireAuth $ maybe (return ()) partChannel currentChannel
-    userCommand $ \"admin" "set" "nick" name -> requireAuth $ changeNickname name
-    userCommand $ \"admin" "help"            -> showHelp
+    userCommand' $ \text -> case words text of
+      ["admin", "auth", pw] -> auth pw
+      ["admin", "join", channel] -> requireAuth $ joinChannel channel
+      ["admin", "part", channel] -> doneAfter $ requireAuth $ partChannel channel
+      ["admin", "part"] -> doneAfter $ requireAuth $ maybe (return ()) partChannel currentChannel
+      ["admin", "set", "nick", name] -> requireAuth $ changeNickname name
+      ["admin", "help"] -> showHelp
+      _ -> return ()
 
 showHelp :: AdminM ()
 showHelp = do
@@ -47,25 +48,25 @@ auth pw = withUsername $ \un -> do
   response <- update' $ Authenticate un pw
   createCheckpoint'
   answer response
- where
-  update' u = do
-    as <- ask
-    liftIO $ AS.update as u
-  createCheckpoint' = do
-    as <- ask
-    liftIO $ AS.createCheckpoint as
+  where
+    update' u = do
+      as <- ask
+      liftIO $ AS.update as u
+    createCheckpoint' = do
+      as <- ask
+      liftIO $ AS.createCheckpoint as
 
 requireAuth :: AdminM () -> AdminM ()
 requireAuth m = do
   withUsername $ \uname -> do
     ia <- query' $ IsAdmin uname Nothing
     if ia
-       then m
-       else answer "You don't have permission to do that."
- where
-  query' q = do
-    as <- ask
-    liftIO $ AS.query as q
+      then m
+      else answer "You don't have permission to do that."
+  where
+    query' q = do
+      as <- ask
+      liftIO $ AS.query as q
 
 --------------------------------------------------------------------------------
 -- Module instances
@@ -73,17 +74,17 @@ requireAuth m = do
 adminModule :: AdminSettings -> Module
 adminModule = newModule . AdminModule
 
-newtype AdminModule = AdminModule { unAM :: AdminSettings }
+newtype AdminModule = AdminModule {unAM :: AdminSettings}
 
 type AdminM a = ModuleMessageM AdminModule a
 
 instance IsModule AdminModule where
   type ModuleState AdminModule = AcidState AdminSettings
-  moduleName     _ = "Admin"
-  initModule     a = openLocalState (unAM a)
-  shutdownModule _ = Just closeAcidState 
-  onStartup      _ = Nothing
-  onMessage      _ = Just runAdminModule
+  moduleName _ = "Admin"
+  initModule a = openLocalState (unAM a)
+  shutdownModule _ = Just closeAcidState
+  onStartup _ = Nothing
+  onMessage _ = Just runAdminModule
 
 emptyAdminSettings :: AdminSettings
 emptyAdminSettings = AdminSettings [] M.empty Nothing
