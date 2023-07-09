5a9b069d3afd8611ac419b9c09d11f9aa9bd19e9
Nils <mail@nils.cc>
2023-07-09 16:22:07 +0200

    Auto format

--- app/Hirc/Modules/Admin/Acid.hs
+++ app/Hirc/Modules/Admin/Acid.hs
@@ -1,23 +1,24 @@
-{-# LANGUAGE PatternGuards, TemplateHaskell, TypeFamilies, DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE PatternGuards #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Hirc.Modules.Admin.Acid where
 
-import Data.Acid
-import Control.Monad.State
 import Control.Monad.Reader
+import Control.Monad.State
+import Data.Acid
 import qualified Data.Map as M
-
 import Hirc hiding (ask, asks)
 
 type Password = String
 
 data AdminSettings = AdminSettings
-  { globalAdmins  :: [UserName]
-  , localAdmins   :: M.Map ChannelName [UserName]
-  , adminPassword :: Maybe String
+  { globalAdmins :: [UserName],
+    localAdmins :: M.Map ChannelName [UserName],
+    adminPassword :: Maybe String
   }
   deriving (Typeable)
-
 
 --------------------------------------------------------------------------------
 -- ACID functions
@@ -26,20 +27,20 @@ isAdmin :: UserName -> Maybe ChannelName -> Query AdminSettings Bool
 isAdmin un mc = do
   as <- ask
   let ga = globalAdmins as
-      la = localAdmins  as
+      la = localAdmins as
   return $ un `elem` ga || un `elem` maybe [] (la M.!) mc
 
 authenticate :: UserName -> Password -> Update AdminSettings String
 authenticate un pw = do
   as <- get -- adminPassword
   case adminPassword as of
-       Nothing -> return "Password authentication disabled."
-       Just apw
-         | pw == apw -> do
-           modify $ \as' -> as' { globalAdmins = un : globalAdmins as' }
-           return "You're successfully authenticated."
-         | otherwise ->
-           return "Incorrect password."
+    Nothing -> return "Password authentication disabled."
+    Just apw
+      | pw == apw -> do
+          modify $ \as' -> as' {globalAdmins = un : globalAdmins as'}
+          return "You're successfully authenticated."
+      | otherwise ->
+          return "Incorrect password."
 
 {-
 authenticateLocal :: Username -> ChannelName -> Password -> Update AdminSettings (MessageM ())
@@ -57,8 +58,8 @@ authenticateLocal un ch pw = do
 
 deriveSafeCopy 0 'base ''AdminSettings
 
-makeAcidic ''AdminSettings
-           [ 'isAdmin
-           , 'authenticate
-           ]
-
+makeAcidic
+  ''AdminSettings
+  [ 'isAdmin,
+    'authenticate
+  ]
