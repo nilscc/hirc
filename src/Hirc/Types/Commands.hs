2226a9ad2615ccc2fbd4b470e2ee0b993b48cda4
Nils <mail@nils.cc>
2023-07-09 02:53:40 +0200

    Reformat and ignore HLINT issue

--- src/Hirc/Types/Commands.hs
+++ src/Hirc/Types/Commands.hs
@@ -1,13 +1,18 @@
-{-# LANGUAGE ScopedTypeVariables, FlexibleInstances,
-             MultiParamTypeClasses, GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use camelCase" #-}
 
 module Hirc.Types.Commands where
 
 data HircCommand m
   = HC_Nothing
-  | HC_Run          (m (HircCommand m))
-  | HC_Lam          (String   -> HircCommand m)
-  | HC_Lams         ([String] -> HircCommand m)
+  | HC_Run (m (HircCommand m))
+  | HC_Lam (String -> HircCommand m)
+  | HC_Lams ([String] -> HircCommand m)
 
 class IsHircCommand m a where
   toCmd :: a -> HircCommand m
